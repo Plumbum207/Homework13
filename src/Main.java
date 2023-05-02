d4c6f68c01350268f888db6b451aa40394abc6aa
Vladimir <plumbumtrident@gmail.com>
2023-05-03 03:34:09 +0700

    воспользовался шаблонными методами...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author tolstoy = new Author("Лев", "Толстой");
+        Author dostoevsky = new Author("Фёдор", "Достоевский");
+
+        Book warPeace = new Book("Война и мир", tolstoy, 1867);
+        Book idiot = new Book("Идиот", dostoevsky, 1869);
+
+        warPeace.setPublishingYear(1869);
+
+        System.out.println(warPeace.toString());
+        System.out.println(warPeace.equals(idiot));
+        System.out.println(warPeace.hashCode());
+
     }
 }
